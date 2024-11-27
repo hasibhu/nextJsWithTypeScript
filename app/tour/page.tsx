e05c8ae65e7dd942ed751f1fb99c29d0754e5366
hasibhu <hasibul1987@gmail.com>
2024-11-27 11:55:40 +0100

    tour component has been created

---
+++ app/tour/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const url = 'https://www.course-api.com/react-tours-project';
+
+type Tour = {
+    id: string;
+    name: string;
+    info: string;
+    image: string;
+    price: string;
+}
+
+
+
+const Tour = () => {
+
+
+
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Tour;
