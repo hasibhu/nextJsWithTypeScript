eeb9291e41f1548d292c34a073413fa3d4781bfd
hasibhu <hasibul1987@gmail.com>
2024-11-27 11:32:22 +0100

    Counter component has been implemented

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 
 const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    
     return (
-        <div>
-            <h2>Counter is here </h2>
+        <div className='mx-auto'>
+            <h2 className='text-center'>Counter is here </h2>
+
+            <p className='text-center text-2xl font-bold'>{count}</p>
+            
+
+            <button className='text-center ml-5 bg-yellow-400 p-1 rounded-xl' onClick={()=> setCount(count+1)}>increment</button>
+            <button className='text-center ml-5 bg-yellow-400 p-1 rounded-xl' onClick={()=> setCount(count-1)}>decrement</button>
         </div>
     );
 };
